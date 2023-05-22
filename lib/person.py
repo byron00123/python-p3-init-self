bd56dd704aa31b6fc4e064546870ac1df0fb3cd0
Byron Omondi <omondibyron25@gmail.com>
2023-05-22 21:38:10 +0300

    complete assingment

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Person:
+    def __init__(self, name=None):
+        self.name = name
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
     pass
